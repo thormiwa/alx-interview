08effe04ef779141b0d198cc6ee3e941f73e2905
thormiwa <70862950+thormiwa@users.noreply.github.com>
2021-07-20 15:31:34 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,18 +3,23 @@
 
 
 def pascal_triangle(n):
-   trow = []
-   if n <= 0:
-       return trow
-   for x in range(1, n+1):
-       row = []
-       for a in range(x):
-           if a == 0 or a == x-1:
-               n = 1
-               row.append(n)
-           else:
-                n = trow[x-2][a-1] + trow[x-2][a]
-                row.append(n)
-       trow.append(row)
-
-   return trow
+    """print pascal"""
+    ls_1 = []
+    if n <= 0:
+        return ls_1
+    for nu in range(0, n):
+        if nu == 0:
+            ls_1.append([1])
+        else:
+            ls_2 = []
+            len_last = len(ls_1[-1])
+            idx = len(ls_1) - 1
+            for nu_2 in range(0, len_last):
+                if nu_2 == 0:
+                    ls_2.append(1)
+                if nu_2 == len_last - 1:
+                    ls_2.append(1)
+                else:
+                    ls_2.append(ls_1[idx][nu_2] + ls_1[idx][nu_2 + 1])
+            ls_1.append(ls_2)
+    return ls_1
